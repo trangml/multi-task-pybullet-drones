3c9376730da7a6cb3954eafdabae5286aadcaede
trangml <mattluutrang@vt.edu>
2022-08-31 22:33:37 -0400

    ddpg working for crossobs

--- experiments/learning/ddpg.py
+++ experiments/learning/ddpg.py
@@ -38,7 +38,7 @@ def parse_args():
         help="weather to capture videos of the agent performances (check out `videos` folder)")
 
     # Algorithm specific arguments
-    parser.add_argument("--env-id", type=str, default="HopperBulletEnv-v0",
+    parser.add_argument("--env-id", type=str, default="cross-obstacles-aviary-v0",
         help="the id of the environment")
     parser.add_argument("--total-timesteps", type=int, default=1000000,
         help="total timesteps of the experiments")
