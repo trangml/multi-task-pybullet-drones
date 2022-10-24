32b29d440b3f7c34f7e81b0540156415fdda5bab
trangml <mattluutrang@vt.edu>
2022-10-23 22:17:54 -0400

    update collision reward

--- gym_pybullet_drones/envs/single_agent_rl/rewards/CollisionReward.py
+++ gym_pybullet_drones/envs/single_agent_rl/rewards/CollisionReward.py
@@ -6,15 +6,22 @@ import numpy as np
 import pybullet as p
 
 from gym_pybullet_drones.envs.single_agent_rl.rewards.Reward import (
-    DenseReward, SparseReward)
+    DenseReward,
+    SparseReward,
+)
 from gym_pybullet_drones.envs.single_agent_rl.rewards.utils import (
-    NEGATIVE_REWARD, POSITIVE_REWARD, ZERO_REWARD, Bounds, within_bounds)
+    NEGATIVE_REWARD,
+    POSITIVE_REWARD,
+    ZERO_REWARD,
+    Bounds,
+    within_bounds,
+)
 
 
 class CollisionReward(SparseReward):
     """Calculate the sparse collision reward, which is a punishment."""
 
-    def __init__(self, scale, area, client=0):
+    def __init__(self, scale, area=None, client=0):
         """
         Generate
 
@@ -29,23 +36,50 @@ class CollisionReward(SparseReward):
         """
         super().__init__(scale)
         self.CLIENT = client
-        self.area = (Bounds(area[0][0], area[0][1]), Bounds(area[1][0], area[1][1]))
+        self.area = (
+            (Bounds(area[0][0], area[0][1]), Bounds(area[1][0], area[1][1]))
+            if area
+            else None
+        )
+        self.checkArea = True if area else False
+        self.exclude = []
 
     def setClient(self, client):
         self.CLIENT = client
 
+    def defineBody(self, body_id: int):
+        self.exclude.append(body_id)
+
+    def _getExcludedContacts(self, drone_id):
+        excluded_contact_pts = []
+        for body_id in self.exclude:
+            excluded_contact_pts.extend(
+                p.getContactPoints(
+                    bodyA=drone_id, bodyB=body_id, physicsClientId=self.CLIENT
+                )
+            )
+        return excluded_contact_pts
+
     def _calculateReward(self, state, drone_id):
         # For now, ignore height.
         contact_pts = p.getContactPoints(bodyA=drone_id, physicsClientId=self.CLIENT)
+        # if there are contact points
         if len(contact_pts) > 0:
-            # TODO: Update with things which can be collided without penalty ie, landing zone
-            position = state[0:2]
-            in_area = True
-            for bound, pos in zip(self.area, position):
-                in_area = in_area and within_bounds(bound, pos)
+            # check the excluded contact points
+            excluded_contact_pts = self._getExcludedContacts(drone_id)
+            if len(excluded_contact_pts) != len(contact_pts):
+                position = state[0:2]
+                if self.checkArea:
+                    # check if we're in a safe area
+                    in_area = True
+                    for bound, pos in zip(self.area, position):
+                        in_area = in_area and within_bounds(bound, pos)
 
-            if not in_area:
-                return NEGATIVE_REWARD
+                    if not in_area:
+                        return NEGATIVE_REWARD
+                else:
+                    # if we don't have a safe area, we're done
+                    return NEGATIVE_REWARD
         return ZERO_REWARD
 
     ################################################################################
